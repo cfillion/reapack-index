908bac1a31fac802446357850e31e152a772dbf8
cfillion <cfillion@users.noreply.github.com>
2016-02-11 02:37:51 -0500

    mild refactoring

--- lib/reapack/index/metadata.rb
+++ lib/reapack/index/metadata.rb
@@ -1,52 +1,6 @@
 class ReaPack::Index
   class Metadata
     TAG = 'metadata'.freeze
-    LINK = 'link'.freeze
-    TYPE = 'rel'.freeze
-    URL = 'href'.freeze
-
-    # the first type will be the default one
-    VALID_TYPES = [:website, :donation].freeze
-
-    Link = Struct.new :name, :url, :is_new?, :modified? do
-      def self.from_node(node)
-        name, url = node.text.to_s, node[URL].to_s
-        url, name = name, url if url.empty?
-        name = url if name.empty?
-
-        self.new name, url, false, false
-      end
-
-      def self.check_type(type)
-        raise ArgumentError unless VALID_TYPES.include? type
-      end
-
-      def self.same_type?(type, user)
-        # match invalid types by the first value of VALID_TYPES
-        # while the other values require an exact match
-        user == type || (type == VALID_TYPES[0] && VALID_TYPES.index(user).to_i < 1)
-      end
-
-      def self.find_all(type, parent)
-        Link.check_type type
-
-        return [] unless parent
-
-        parent.element_children.select {|node|
-          node.name == LINK && Link.same_type?(type, node[TYPE].to_s.to_sym)
-        }
-      end
-
-      def self.find(type, search, parent)
-        Link.find_all(type, parent).find {|node|
-          node.text == search || node[URL] == search
-        }
-      end
-
-      def ==(other)
-        name == other.name && url == other.url
-      end
-    end
 
     def initialize(parent)
       @parent = parent
@@ -75,22 +29,22 @@ class ReaPack::Index
       node ||= Link.find type, link.url, @root
 
       if node
-        link[:'is_new?'] = false
-        link[:'modified?'] = link != Link.from_node(node)
+        link.instance_variable_set :@is_new, false
+        link.instance_variable_set :@modified, link != Link.from_node(node)
 
-        node.remove_attribute URL
+        node.remove_attribute Link::URL
       else
-        link[:'is_new?'] = true
-        link[:'modified?'] = true
+        link.instance_variable_set :@is_new, true
+        link.instance_variable_set :@modified, true
 
-        node = Nokogiri::XML::Node.new LINK, @root.document
+        node = Nokogiri::XML::Node.new Link::TAG, @root.document
         node.parent = @root
-        node[TYPE] = type
+        node[Link::REL] = type
       end
 
 
       if name
-        node[URL] = url
+        node[Link::URL] = url
         node.content = name
       else
         node.content = url
@@ -124,6 +78,68 @@ class ReaPack::Index
 
     def auto_remove
       @root.remove if @root.children.empty?
+    end
+  end
+
+  class Link
+    TAG = 'link'.freeze
+    REL = 'rel'.freeze
+    URL = 'href'.freeze
+
+    # the first type will be the default one
+    VALID_TYPES = [:website, :donation].freeze
+
+    def self.from_node(node)
+      name, url = node.text.to_s, node[URL].to_s
+      url, name = name, url if url.empty?
+      name = url if name.empty?
+
+      self.new name, url
+    end
+
+    def self.check_type(type)
+      raise ArgumentError unless VALID_TYPES.include? type
+    end
+
+    def self.same_type?(type, user)
+      # match invalid types by the first value of VALID_TYPES
+      # while the other values require an exact match
+      user == type || (type == VALID_TYPES[0] && VALID_TYPES.index(user).to_i < 1)
+    end
+
+    def self.find_all(type, parent)
+      Link.check_type type
+
+      return [] unless parent
+
+      parent.element_children.select {|node|
+        node.name == TAG && Link.same_type?(type, node[REL].to_s.to_sym)
+      }
+    end
+
+    def self.find(type, search, parent)
+      Link.find_all(type, parent).find {|node|
+        node.text == search || node[URL] == search
+      }
+    end
+
+    def initialize(name, url)
+      @name, @url = name, url
+      @is_new = @modified = false
+    end
+
+    attr_accessor :name, :url
+
+    def ==(other)
+      name == other.name && url == other.url
+    end
+
+    def is_new?
+      @is_new
+    end
+
+    def modified?
+      @modified
     end
   end
 end
