cfillion/reapack-index

commit 7a6259e227896deabedfb68c5fd3bb38b952e316

update to metaheader v2.0.0

lib/reapack/index.rb
@@ -120,8 +120,16 @@ class ReaPack::Index
     type = self.class.type_of path
     return unless type
 
-    mh = MetaHeader.parse contents
-    mh.strict = @strict
+    mh = if contents.is_a? MetaHeader
+      contents
+    else
+      mh = MetaHeader.parse contents
+      unless mh.has? :changelog
+        wp = WordpressChangelog.new mh
+        wp.parse contents
+      end
+      mh
+    end
 
     if mh[:noindex]
       remove path
@@ -139,7 +147,7 @@ class ReaPack::Index
     scanner = Scanner.new cat, pkg, mh, self
 
     begin
-      scanner.run
+      scanner.run @strict
     rescue Error
       backups.each {|var, value| instance_variable_set var, value }
       raise

lib/reapack/index/git.rb
@@ -232,7 +232,7 @@ class ReaPack::Index
     end
 
     def new_header
-      @new_header ||= MetaHeader.new @new_content if new_content
+      @new_header ||= MetaHeader.parse @new_content if new_content
     end
   end
 end

lib/reapack/index/parsers.rb
@@ -1,4 +1,4 @@
-class WordpressChangelog < MetaHeader::Parser
+class WordpressChangelog
   CHANGELOG = /
     Changelog\s*:\n
     (.+?)\n\s*
@@ -7,11 +7,14 @@ class WordpressChangelog < MetaHeader::Parser
 
   VERSION = /\A[\s\*]*v([\d\.]+)(?:\s+(.+))?\Z/.freeze
 
+  def initialize(mh)
+    @header = mh
+  end
+
   def parse(input)
-    input = input.read
     input.encode! Encoding::UTF_8, invalid: :replace
 
-    ver, changes = header[:version], header[:changelog]
+    ver, changes = @header[:version], @header[:changelog]
     return if ver.nil? || changes || CHANGELOG.match(input).nil?
 
     $1.lines.each {|line| read line.lstrip }
@@ -19,15 +22,15 @@ class WordpressChangelog < MetaHeader::Parser
 
   def read(line)
     if line =~ VERSION
-      @current = $1 == header[:version]
+      @current = $1 == @header[:version]
     elsif @current
-      if header[:changelog]
-        header[:changelog] += "\n"
+      if @header[:changelog]
+        @header[:changelog] += "\n"
       else
-        header[:changelog] = String.new
+        @header[:changelog] = String.new
       end
 
-      header[:changelog] += line.chomp
+      @header[:changelog] += line.chomp
     end
   end
 end

lib/reapack/index/scanner.rb
@@ -52,10 +52,12 @@ class ReaPack::Index
       @self_overriden = false
     end
 
-    def run
+    def run(strict = false)
       @mh.alias HEADER_ALIASES
 
-      if errors = @mh.validate(HEADER_RULES)
+      errors = @mh.validate(HEADER_RULES, strict)
+
+      unless errors.empty?
         raise Error, errors.join("\n")
       end
 

reapack-index.gemspec
@@ -29,7 +29,7 @@ Gem::Specification.new do |spec|
   spec.add_runtime_dependency 'addressable', '~> 2.4'
   spec.add_runtime_dependency 'colorize', '~> 0.7'
   spec.add_runtime_dependency 'gitable', '~> 0.3'
-  spec.add_runtime_dependency 'metaheader', '~> 1.3'
+  spec.add_runtime_dependency 'metaheader', '~> 2.0'
   spec.add_runtime_dependency 'nokogiri', '~> 1.7'
   spec.add_runtime_dependency 'pandoc-ruby', '~> 2.0'
   spec.add_runtime_dependency 'rugged', '~> 0.25'

test/scanner/test_validation.rb
@@ -8,7 +8,7 @@ class TestScanner::TestValidation < MiniTest::Test
     @pkg.expect :type, :script
     @pkg.expect :path, 'cat/test'
 
-    @mh = MetaHeader.new String.new
+    @mh = MetaHeader.new
     @mh[:version] = '1.0'
 
     @index = MiniTest::Mock.new
@@ -20,7 +20,7 @@ class TestScanner::TestValidation < MiniTest::Test
   def test_validation
     mh_mock = MiniTest::Mock.new
     mh_mock.expect :alias, nil, [Hash]
-    mh_mock.expect :validate, ['first', 'second'], [Hash]
+    mh_mock.expect :validate, ['first', 'second'], [Hash, false]
 
     @scanner.instance_variable_set :@mh, mh_mock
 

test/test_parsers.rb
@@ -2,7 +2,7 @@ require File.expand_path '../helper', __FILE__
 
 class TestParsers < MiniTest::Test
   def test_wordpress
-    mh = MetaHeader.new <<-IN
+    input = <<-IN
 /**
  * Version: 1.1
  */
@@ -23,12 +23,17 @@ class TestParsers < MiniTest::Test
  Test\x97
     IN
 
+    mh = MetaHeader.parse input
     assert_equal '1.1', mh[:version]
+    refute mh.has?(:changelog)
+
+    parser = WordpressChangelog.new mh
+    parser.parse input
     assert_equal "+ Line 3\n+ Line 4", mh[:changelog]
   end
 
   def test_wordpress_no_date
-    mh = MetaHeader.new <<-IN
+    input = <<-IN
 /**
  * Version: 1.1
  */
@@ -49,12 +54,14 @@ class TestParsers < MiniTest::Test
  Test\x97
     IN
 
-    assert_equal '1.1', mh[:version]
+    mh = MetaHeader.parse input
+    parser = WordpressChangelog.new mh
+    parser.parse input
     assert_equal "+ Line 3\n+ Line 4", mh[:changelog]
   end
 
   def test_wordpress_noprefix
-    mh = MetaHeader.new <<-IN
+    input = <<-IN
 --[[
 Version: 1.1
 --]]
@@ -76,7 +83,9 @@ v1.0 (2012-01-01)
  Test\x97
     IN
 
-    assert_equal '1.1', mh[:version]
+    mh = MetaHeader.parse input
+    parser = WordpressChangelog.new mh
+    parser.parse input
     assert_equal "+ Line 3\n+ Line 4", mh[:changelog]
   end
 end

test/test_scanner.rb
@@ -6,7 +6,7 @@ class TestScanner < MiniTest::Test
   def setup
     @mock = MiniTest::Mock.new
 
-    @mh = MetaHeader.new String.new
+    @mh = MetaHeader.new
     @mh[:version] = '1.0'
 
     @doc = Nokogiri::XML <<-XML

commit 8ff2724c1710dbf2c1603c2ef0dd46e6020b68bb

fix parsing of the deprecated compatibility changelog format [p=2278030]
regression from 7a6259e227896deabedfb68c5fd3bb38b952e316

lib/reapack/index.rb
@@ -77,6 +77,15 @@ class ReaPack::Index
         .find {|name, exts| input.to_sym == name || exts.include?(input.to_s) }
         &.first
     end
+
+    def parse(contents)
+      mh = MetaHeader.parse contents
+      unless mh.has? :changelog
+        wp = WordpressChangelog.new mh
+        wp.parse contents
+      end
+      mh
+    end
   end
 
   def initialize(path)
@@ -120,16 +129,7 @@ class ReaPack::Index
     type = self.class.type_of path
     return unless type
 
-    mh = if contents.is_a? MetaHeader
-      contents
-    else
-      mh = MetaHeader.parse contents
-      unless mh.has? :changelog
-        wp = WordpressChangelog.new mh
-        wp.parse contents
-      end
-      mh
-    end
+    mh = contents.is_a?(MetaHeader) ? contents : self.class.parse(contents)
 
     if mh[:noindex]
       remove path

lib/reapack/index/git.rb
@@ -232,7 +232,7 @@ class ReaPack::Index
     end
 
     def new_header
-      @new_header ||= MetaHeader.parse @new_content if new_content
+      @new_header ||= ReaPack::Index.parse @new_content if new_content
     end
   end
 end